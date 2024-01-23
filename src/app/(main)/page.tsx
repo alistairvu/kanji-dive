14e469203421b2b2dd85a01d09b5f0c822b0ab78
Alistair Vu <alistairvu@gmail.com>
2024-01-23 17:52:47 +0700

    Finish landing page

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -2,6 +2,8 @@ import Link from "next/link";
 
 import { getServerAuthSession } from "@/server/auth";
 import { redirect } from "next/navigation";
+import { cn } from "@/lib/utils";
+import { buttonVariants } from "@/components/ui/button";
 
 export default async function Home() {
   const session = await getServerAuthSession();
@@ -12,16 +14,27 @@ export default async function Home() {
 
   return (
     <main className="container my-4">
-      <h1 className="text-6xl font-semibold">Learn. Wait. Review</h1>
+      <h1 className="text-6xl font-semibold">Learn. Wait. Review.</h1>
 
-      <p className="mt-2">
+      <p className="my-4 text-2xl">
         Dive into the world of Kanji with KanjiDive, where you can easily learn
         and review thousands of Kanji characters and vocabulary words containing
-        Kanji. Utilizing the spaced repetition method, KanjiDive helps you
-        retain information longer without overwhelming you. Whether you're a
-        beginner or a Kanji master, you'll find learning Kanji more engaging and
-        effective than ever. Ready to plunge into the ocean of Kanji characters?
+        Kanji.
       </p>
+      <p className="my-4 text-2xl">
+        Utilizing a smart spaced repetition algorithm, KanjiDive helps you
+        retain information longer without overwhelming you.
+      </p>
+      <p className="my-4 text-2xl">
+        Ready to plunge into the ocean of Kanji characters?
+      </p>
+
+      <Link
+        href="/api/auth/signin"
+        className={cn(buttonVariants({ variant: "default" }), "mt-2")}
+      >
+        Sign in with Discord
+      </Link>
     </main>
   );
 }
