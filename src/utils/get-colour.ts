e2bbabb0a2d19aa45bb822ebbbee8b631dce7f68
Alistair Vu <alistairvu@gmail.com>
2024-01-23 20:28:11 +0700

    Edit title

--- src/utils/get-colour.ts
+++ src/utils/get-colour.ts
@@ -2,7 +2,6 @@ import { NoteType } from "@prisma/client";
 
 export function getColour(type: NoteType) {
   return {
-    "bg-yellow-200 text-amber-950 shadow-yellow-400": type === NoteType.RADICAL,
     "bg-blue-500 text-sky-50 shadow-blue-400": type === NoteType.KANJI,
     "bg-red-500 text-red-50 shadow-red-400": type === NoteType.VOCAB,
   };
